7317fdfc562dc0eb864bad51999f302ce32edb27
Tronze <0183535663@naver.com>
2024-10-04 17:48:10 +0900

    feat: User App 추가 및 User Model 추가.

---
+++ backend/account/manager.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+# Create your manager here.
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+            name=name,
+        )
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
