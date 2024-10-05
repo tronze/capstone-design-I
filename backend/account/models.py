270d549cdda5a690d6abb177544f9ce0b5ded297
YOOHOO <yoohoo_030@naver.com>
2024-10-05 15:35:13 +0900

    fix: User 모델의 REQUIRED_FIELDS 수정
    'first_name', 'last_name' 제거 및 'name' 추가

--- backend/account/models.py
+++ backend/account/models.py
@@ -33,7 +33,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    REQUIRED_FIELDS = ['name'] 
 
     class Meta:
         ordering = ('-date_joined',)
