debae504991be080932386d6ae364c7e80ad6841
YOOHOO <yoohoo_030@naver.com>
2024-10-05 14:45:40 +0900

    chore: settings.py에 custom usermodel 등록

--- backend/backend_module/settings/base.py
+++ backend/backend_module/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -101,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'account.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
