97a1d3518612f3ae4c31d68782ded2519e4c41a1
YOOHOO <yoohoo_030@naver.com>
2024-10-14 00:52:16 +0900

    feat: IntegrityError와 ValuError 발생 시 커스텀에러핸들러가 처리하도록 설정 및 세부 에러 핸들링(Unique constraint, ValueError)

--- backend/backend_module/custom_exception_handler.py
+++ backend/backend_module/custom_exception_handler.py
@@ -1,10 +1,23 @@
 from rest_framework.views import exception_handler
+from rest_framework.response import Response
+from rest_framework import status
+from django.db import IntegrityError
+
 
 def custom_exception_handler(exc, context):
     # Call REST framework's default exception handler first,
     # to get the standard error response.
     response = exception_handler(exc, context)
 
+    if response is None:
+        if isinstance(exc, IntegrityError):
+            if "UNIQUE constraint failed: account_user.email" in str(exc):
+                return Response({"email": "중복된 이메일입니다. 다른 이메일을 입력해주세요.", "status_code": status.HTTP_400_BAD_REQUEST}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": str(exc), "status_code": status.HTTP_400_BAD_REQUEST}, status=status.HTTP_400_BAD_REQUEST)
+        if isinstance(exc, ValueError):
+            if "ValueError: Invalid user type" in str(exc):
+                return Response({"user_type": "유효하지 않은 user_type입니다.", "status_code": status.HTTP_400_BAD_REQUEST})
+            return Response({'error': str(exc), "status_code": status.HTTP_400_BAD_REQUEST}, status=status.HTTP_400_BAD_REQUEST)
     # Now add the HTTP status code to the response.
     if response is not None:
         response.data['status_code'] = response.status_code
